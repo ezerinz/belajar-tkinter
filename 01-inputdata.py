6d39097e677ae3be43d3e34203388baf37c1f413
Ezerinz <edwinnnzx@gmail.com>
2022-11-27 14:59:38 +0800

    changes again

---
+++ 01-inputdata.py
@@ -0,0 +1,60 @@
+from tkinter import *
+
+nama = []
+umur = []
+root = Tk()
+root.geometry("350x350")
+
+frameInput = Frame(root)
+frameInput.grid(row=0, column=0)
+
+frameList = Frame(root)
+frameList.grid(row=1, column=0)
+
+
+lbl1 = Label(frameInput, text="Masukkan Nama: ")
+lbl1.grid(row= 0, column=0)
+
+etr1 = Entry(frameInput)
+etr1.grid(row=0, column=1)
+
+
+def tmbhnama():
+    getNama = etr1.get()
+    nama.append(getNama)
+    nma = Listbox(frameList)
+
+    for i in range(len(nama)):
+        nma.insert(i+1, nama[i])
+
+    nma.grid(row=0, column=0)
+
+
+btn1 = Button(frameInput, text=" + ", command = tmbhnama)
+btn1.grid(row=0, column=2)
+
+
+
+lbl2 = Label(frameInput, text="Masukkan Umur: ")
+lbl2.grid(row= 1, column=0)
+
+etr2 = Spinbox(frameInput, from_=1, to=50, width=19)
+etr2.grid(row=1, column=1)
+
+
+def tmbhumur():
+    getUmur = etr2.get()
+    umur.append(getUmur)
+    umr = Listbox(frameList)
+
+    for i in range(len(umur)):
+        umr.insert(i+1, umur[i])
+
+    umr.grid(row=0, column=1)
+
+
+btn2 = Button(frameInput, text=" + ", command = tmbhumur)
+btn2.grid(row=1, column=2)
+
+
+root.mainloop()

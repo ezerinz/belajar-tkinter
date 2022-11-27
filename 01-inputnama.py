9dc6c0ed38b95d0a8390d22345a03dff6bf3c390
Ezerinz <edwinnnzx@gmail.com>
2022-11-27 12:17:43 +0800

    changes

--- 01-inputnama.py
+++ 01-inputnama.py
@@ -1,7 +1,8 @@
 from tkinter import *
 
+nama = []
 root = Tk()
-root.geometry("400x300")
+root.geometry("290x300")
 
 lbl = Label(root, text="Masukkan Nama: ")
 lbl.grid(column=0)
@@ -10,19 +11,17 @@ etr = Entry(root)
 etr.grid(row=0, column=1)
 
 def cmd():
-    t = StringVar()
-    t.set(etr.get())
-    Entry(root, textvariable=t).grid(row=3, column=1)
+    getEntry = etr.get()
+    nama.append(getEntry)
+    nma = Listbox(root, width=35)
+
+    for i in range(len(nama)):
+        nma.insert(i+1, nama[i])
+
+    nma.grid(row=2, columnspan=2)
+
 
 btn = Button(root, text="Tambah", command=cmd)
 btn.grid(row=1, column=0)
-
-lbl2 = Label(root, text="Masukkan Nama Kedua: ")
-lbl2.grid(row=3, column=0)
-
-etr2 = Entry(root)
-etr2.grid(row=3, column=1)
-
-
 
 root.mainloop()
